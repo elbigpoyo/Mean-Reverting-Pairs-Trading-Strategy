275ee9c4e55fce9ac04cda92e5b8cff721f4569c
Hansel Chen <hanselchen@10-16-94-204.dynapool.wireless.nyu.edu>
2025-10-25 14:39:56 -0400

    johansen test

--- commodities.py
+++ commodities.py
@@ -1,21 +1,24 @@
 import pandas as pd
-from stationarity_test import adf_test as adf
+from stationarity_test import johansen_test, adf_test
 
 commodities_table = pd.read_csv("commodities.csv")
 print(commodities_table)
 
 ticker_list = commodities_table["YahooFuturesTicker"].tolist()
 
-column_names = ["commodity 1", "commodity 2", "p-value"]
+column_names = ["commodity 1", "commodity 2", "comparison"]
 
-final = pd.DataFrame(columns = column_names)
+final = pd.DataFrame(columns=column_names)
 
-start_date = '2025-01-01'
-end_date = '2025-10-01'
+start_date = "2025-01-01"
+end_date = "2025-10-01"
 
 for i in range(len(ticker_list)):
     for j in range(i + 1, len(ticker_list)):
-        final.loc[len(final)]= [ticker_list[i], ticker_list[j], adf(ticker_list[i], ticker_list[j], start_date, end_date)]
+        final.loc[len(final)] = [
+            ticker_list[i],
+            ticker_list[j],
+            johansen_test(ticker_list[i], ticker_list[j], start_date, end_date),
+        ]
 
-final_sorted = final.sort_values(by = "p-value")
-print(final_sorted.head(20))
+print(final)
--- stationarity_test.py
+++ stationarity_test.py
@@ -1,25 +1,74 @@
 import yfinance as yf
 import pandas as pd
-from statsmodels.tsa.stattools import coint
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import numpy as np
+from statsmodels.tsa.vector_ar.vecm import coint_johansen
+from statsmodels.tsa.vector_ar.var_model import VAR
+from datetime import datetime, timedelta
+
+start_date = "2000-10-01"
+end_date = "2025-10-01"
 
 
-#asset1 = 'USO'
-#asset2 = 'SLV'
+def adf_test(price_series):
+    adf_result = sm.tsa.stattools.adfuller(price_series)
+    p_value = adf_result[1]
+    return p_value < 0.05
 
-start_date = '2023-10-01'
-end_date = '2025-10-01'
-def adf_test(asset1, asset2, start_date, end_date):
-    asset1_price_series = yf.download(asset1, start=start_date, end=end_date, auto_adjust=False, progress = True)['Adj Close']
-    asset2_price_series = yf.download(asset2, start=start_date, end=end_date, auto_adjust=False, progress = True)['Adj Close']
 
-    log_asset1 = np.log(asset1_price_series)
-    log_asset2 = np.log(asset2_price_series)
-    merged_price_series = pd.merge(log_asset1, log_asset2, on="Date", how="outer").dropna()
+def optimal_lag_selection(time_series):
+    model = VAR(time_series)
+    lag_order_results = model.select_order(maxlags=10)
+    optimal_lag = lag_order_results.aic
+    return optimal_lag - 1
 
-    coint_result = coint(merged_price_series[asset1], merged_price_series[asset2], autolag= 'AIC', trend = 'ct')
 
-    p_value = coint_result[1]
-    return p_value
+def johansen_test(asset1, asset2, start_date, end_date):
+    asset1_price_series = yf.download(
+        asset1, start=start_date, end=end_date, auto_adjust=False
+    )["Adj Close"]
+    asset2_price_series = yf.download(
+        asset2, start=start_date, end=end_date, auto_adjust=False
+    )["Adj Close"]
+    if adf_test(asset1_price_series) or adf_test(asset2_price_series):
+        return [-1, -1]
 
+    merged_price_series = pd.merge(
+        asset1_price_series, asset2_price_series, on="Date", how="outer"
+    ).dropna()
+
+    johansen_result = coint_johansen(
+        merged_price_series,
+        det_order=0,
+        k_ar_diff=optimal_lag_selection(merged_price_series),
+    )
+
+    test_stat_trace_r0 = johansen_result.lr1[0]
+    critical_value_trace_r0 = johansen_result.cvt[0, 1]
+    return [
+        float(test_stat_trace_r0),
+        float(critical_value_trace_r0),
+    ], merged_price_series
+
+
+def plot_cointegration(merged_date, asset1, asset2):
+    model = sm.OLS(merged_date[asset1], sm.add_constant(merged_date[asset2])).fit()
+    hedge_ratio = model.params[asset2]
+    spread = merged_date[asset1] - hedge_ratio * merged_date[asset2]
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(merged_date.index, spread, label="Spread", color="blue")
+    plt.axhline(spread.mean(), color="red", linestyle="--", label="Mean")
+    plt.title(f"Cointegration Spread between {asset1} and {asset2}")
+    plt.xlabel("Date")
+    plt.ylabel("Spread")
+    plt.legend()
+    plt.show()
+
+
+asset1 = "GLD"
+asset2 = "BTC-USD"
+result, merged_data = johansen_test(asset1, asset2, start_date, end_date)
+print(result)
+plot_cointegration(merged_data, asset1, asset2)
